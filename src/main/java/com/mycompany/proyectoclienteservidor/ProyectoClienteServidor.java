5782700ef3a0d09fd92d3af93f89d7307f9a788d
Justin Berger <jusaleberger@gmail.com>
2023-10-28 18:46:40 -0600

    Cambios nuevos

--- src/main/java/com/mycompany/proyectoclienteservidor/ProyectoClienteServidor.java
+++ src/main/java/com/mycompany/proyectoclienteservidor/ProyectoClienteServidor.java
@@ -13,6 +13,17 @@ public class ProyectoClienteServidor {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Justin Branch");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
+        System.out.println("Nuevas lineas de codigo en Justin");
         
     }
 }
