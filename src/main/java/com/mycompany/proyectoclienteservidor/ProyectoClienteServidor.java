164baa80b7dc385925ee327169e2872b1e66e9ea
Moisés Jiménez <mjimenez60052@ufide.ac.cr>
2023-10-28 18:34:49 -0600

    Commit de prueba

--- src/main/java/com/mycompany/proyectoclienteservidor/ProyectoClienteServidor.java
+++ src/main/java/com/mycompany/proyectoclienteservidor/ProyectoClienteServidor.java
@@ -13,5 +13,7 @@ public class ProyectoClienteServidor {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("MOISES");
+        
+        System.out.println("KKKKKKK");
     }
 }
