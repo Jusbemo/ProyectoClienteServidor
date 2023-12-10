d72f9cd3d3cec4c73b3c7e8ee7e3710a51cd4f0f
Justin Berger <jusaleberger@gmail.com>
2023-12-09 23:59:03 -0600

    Modificacion archivo main

--- src/main/java/com/mycompany/proyectoclienteservidor/ProyectoClienteServidor.java
+++ src/main/java/com/mycompany/proyectoclienteservidor/ProyectoClienteServidor.java
@@ -1,21 +1,10 @@
 package com.mycompany.proyectoclienteservidor;
 
+import Interfaz.InicioSesion;
+
 public class ProyectoClienteServidor {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Justin Branch");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        System.out.println("Nuevas lineas de codigo en Justin");
-        
+        new InicioSesion().setVisible(true);
     }
 }
